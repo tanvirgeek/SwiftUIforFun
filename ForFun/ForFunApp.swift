7331d3627eddcd0eb314524f8cafa8d0516f083c
tanvir <tanvirgeek@gmail.com>
2020-11-05 15:50:34 +0600

    Initial Commit

---
+++ ForFun/ForFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForFunApp.swift
+//  ForFun
+//
+//  Created by MD Tanvir Alam on 5/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ForFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
